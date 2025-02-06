477697965e12d89367cd5822179618e1492fa666
berkeberkay <berkeberkay.tekce@std.yeditepe.edu.tr>
2025-02-06 21:26:50 +0300

    relative import

--- routers/auth.py
+++ routers/auth.py
@@ -23,25 +23,25 @@ ALGORITHM = "HS256"
 def get_db():
     db = SessionLocal()
     try:
-        yield db    # kullanıldığı fonk generator func. denir. returndan farkı birden fazla seq döndürebilir.
+        yield db
     finally:
         db.close()
 
+
 db_dependency = Annotated[Session, Depends(get_db)]
 
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
-
-bcrypt = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 class CreateUserRequest(BaseModel):
     username: str
-    password: str
     email: str
     first_name: str
-    last_name : str
-    role : str
-    is_active : bool
+    last_name: str
+    password: str
+    role: str
+    phone_number: str
 
 
 class Token(BaseModel):
@@ -49,72 +49,66 @@ class Token(BaseModel):
     token_type: str
 
 
-
-def create_access_token(username : str, user_id : int, role : str, exp_delta : timedelta):
-    payload = {"sub": username, "id": user_id, "role": role}  # işime yarayacağını düşündüğüm parametreleri kafama göre aldım.
-    expire = datetime.now(timezone.utc) + exp_delta
-    payload.update({"exp": expire})
+def create_access_token(username: str, user_id: int, role: str, expires_delta: timedelta):
+    payload = {'sub': username, 'id': user_id, 'role': role}
+    expires = datetime.now(timezone.utc) + expires_delta
+    payload.update({'exp': expires})
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
 
-
-
-def authenticate_user(db, username: str, password: str):
+def authenticate_user(username: str, password: str, db):
     user = db.query(User).filter(User.username == username).first()
     if not user:
         return False
-    if not bcrypt.verify(password, user.hashed_password):
+    if not bcrypt_context.verify(password, user.hashed_password):
         return False
     return user
 
-#gidip dependency olarak eklenecek
-#decode eder.
-async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):   #Bizim kullanıcılarımızdan mı diye kontrol ediyorum ki hacker atmış olabilir vs vs.
-    credentials_exception = HTTPException(
-        status_code=401,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        user_id: int = payload.get("id")
-        user_role: str = payload.get("role")
-        if username is None:
-            raise HTTPException(status_code=401, detail="Invalid credentials")
-        return {"username": username, "id": user_id, "role": user_role}
+        username = payload.get('sub')
+        user_id = payload.get('id')
+        user_role = payload.get('role')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Username or ID is invalid")
+        return {'username': username, 'id': user_id, 'user_role': user_role}
     except JWTError:
-        raise credentials_exception
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Token is invalid")
 
 
 @router.get("/login-page")
-def login_page(request: Request):
+def render_login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
+
 @router.get("/register-page")
-def register_page(request: Request):
+def render_register_page(request: Request):
     return templates.TemplateResponse("register.html", {"request": request})
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_user(db : db_dependency, create_user_request: CreateUserRequest):
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     user = User(
         username=create_user_request.username,
         email=create_user_request.email,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
-        hashed_password=bcrypt.hash(create_user_request.password),
+        is_active=True,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        phone_number=create_user_request.phone_number
     )
-
     db.add(user)
     db.commit()
 
 
-@router.post("/token", response_model=Token)
-async def login_for_access_token(form:Annotated[OAuth2PasswordRequestForm,Depends()], db: db_dependency):
-    user = authenticate_user(db,form.username,form.password)
+@router.post("/token", response_model = Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
-    token = create_access_token(user.username, user.id , user.role, timedelta(minutes=30))
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=60))
     return {"access_token": token, "token_type": "bearer"} #bearer yollayan kişi gerçek kullanıcı mı kontrolü yapar.
